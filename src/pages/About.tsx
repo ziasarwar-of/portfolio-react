11808664c9bd9b5e48172c16d7ff9ee5250f4c87
ziasarwar93 <engrehman.93@gmail.com>
2026-05-08 23:07:33 +0500

    fix layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,8 +4,7 @@ function About() {
     modern, responsive, and user-friendly web applications. I specialize in
     React JS, JavaScript, HTML, and CSS, and enjoy transforming ideas into
     clean and interactive digital experiences. I focus on writing clean,
-    maintainable code and building interfaces that provide smooth
-    performance across all devices. Continuously learning new technologies
+    maintainable code and building interfaces that provide smooth performance across all devices. Continuously learning new technologies
     and improving my skills is an important part of my development journey.
   `
     .replace(/\s+/g, " ")
